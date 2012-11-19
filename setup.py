93cb0dae0554e022594b0d93ab059ea99f4f7152
norlowski <nick.orlowski@gmail.com>
2012-11-19 13:26:43 -0500

    Reformatting to accommodate setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='HL7py',
+      version='1.0',
+      description='HL7 message parser',
+      long_description=readme(),
+      keywords='hl7 parsing ',
+      url='https://github.com/norlowski/HL7py',
+      author='Nicholas Orlowski',
+      author_email='nick.orlowski@gmail.com',
+      license='MIT',
+      packages=['HL7py'],
+      zip_safe=False)
